e67b0ce40588b00a8fd89ef87933f1e46230ca24
daffaabhi <widategar@gmail.com>
2020-08-07 22:46:32 +0700

    update pertama

---
+++ src/main/kotlin/Hello kotlin.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val nama = "Daffa Abhi Nandarifka"
+    print("Nama saya")
+    println(nama)
+    val Hobby = "Bermain Game"
+    print("Hobby saya")
+    println(Hobby)
+}
---
+++ src/main/kotlin/If Expression.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>){
+    val nilai = 65
+    if(nilai >= 75){
+        println("Nilai_Kamu " +nilai+ ", selamat Ya!")
+    }else{
+        println("Nilai_kamu " +nilai+ ", silakan ikuti remidial.")
+    }
+}
